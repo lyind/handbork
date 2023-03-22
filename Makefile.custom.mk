ee5cf9b5a2cb06b9cf8d90d762ae3256bbe9c1ec
Marcel <marcel@giantswarm.io>
2023-03-22 17:55:42 +0100

    Remove `HEAD` option to prevent false positives (#34)
    * rebase
    
    * Update Makefile.custom.mk
    
    Co-authored-by: Jonas Zeiger <jonas.zeiger@talpidae.net>
    
    * Revert change to get HEAD as it causes unexpected exit codes
    
    ---------
    
    Co-authored-by: Jonas Zeiger <jonas.zeiger@talpidae.net>

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,4 +1,4 @@
 # This command enables you to quickly check which links to the intranet are dead. \
 Cookie has to be valid to get access through our oauth proxy.
 check-intranet:
-	find . -name \*.md -print0 | xargs -0 -n1 grep -Po "\(\Khttps?://intranet[^\s\)]+" | xargs -I@ -n1 sh -c 'echo @; curl -L -s -o /dev/null -w "%{http_code}\n" -H "Cookie: $(COOKIE)" -X HEAD @'
+	find . -name \*.md -print0 | xargs -0 -n1 grep -Po "\(\Khttps?://intranet[^\s\)]+" | xargs -I@ -n1 sh -c 'echo @; curl -L -s -o /dev/null -w "%{http_code}\n" -H "Cookie: $(COOKIE)" @'
