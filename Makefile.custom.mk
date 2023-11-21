4125de4114708d2a3756f0a4cff854ffcae90f46
Andreas Sommer <andreas@giantswarm.io>
2023-11-21 12:03:00 +0100

    Render RFCs (#175)

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -1,4 +1,17 @@
+# By default, RFCs are read from your computer, assuming the repo (https://github.com/giantswarm/rfc)
+# is cloned on the same directory level as the handbook.
+RFCS_DIR ?= ../rfc
+
 # This command enables you to quickly check which links to the intranet are dead. \
 Cookie has to be valid to get access through our oauth proxy.
 check-intranet:
 	find . -name \*.md -print0 | xargs -0 -n1 grep -Po "\(\Khttps?://intranet[^\s\)]+" | xargs -I@ -n1 sh -c 'echo @; curl -L -s -o /dev/null -w "%{http_code}\n" -H "Cookie: $(COOKIE)" @'
+
+# Aggregate RFCs from other repos
+rfcs:
+	docker run --rm \
+		--volume="$$(realpath "$(RFCS_DIR)"):/rfc:ro" \
+		--volume="${PWD}/scripts:/scripts:ro" \
+		--volume="${PWD}/content/docs/rfcs:/content/docs/rfcs:rw" \
+		quay.io/giantswarm/docs-scriptrunner:latest \
+		/scripts/aggregate-rfcs.py /rfc /content/docs/rfcs
--- README.md
+++ README.md
@@ -9,12 +9,17 @@ For purely internal information we have our [intranet](https://intranet.giantswa
 The `content` folder of this repository is served using the static site generator [HUGO](https://gohugo.io/) docs page.
 It is set up with the [Google docsy](https://github.com/google/docsy) theme and served at [https://handbook.giantswarm.io/](https://handbook.giantswarm.io/).
 
-# Development
+## Development
 
 You can easily test and render any changes to the handbook with:
-```
+
+```sh
 docker-compose build --pull
 docker-compose up
 ```
 
 Content changes are then auto-reloaded in the `docker-compose` setup.
+
+Or for a simplified setup, run `hugo server`.
+
+Auto-generated content needs to be rendered manually (`make rfcs`), as it's normally updated through a GitHub action.
